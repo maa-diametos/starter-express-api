3b5983776b38d8a5ece2ee09cd58e482eaced41d
maa-diametos <161808335+maa-diametos@users.noreply.github.com>
2024-04-01 16:08:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+   console.log("Just got a request!", JSON.stringify(req))
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
