dc8059c3e08f54b7f9b6bcdcb9f1adf1044701d3
maa-diametos <161808335+maa-diametos@users.noreply.github.com>
2024-04-01 17:01:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const url = require('url');
+const querystring = require('querystring');
+
 const app = express()
 app.all('/', (req, res) => {
-   console.log('request url params', req);
+   const parsedUrl = url.parse(req.url);
+   const query = querystring.parse(parsedUrl.query);
+   console.log('request url params', query);
    res.send('Yo!');
 })
 app.listen(process.env.PORT || 3000)
